937593767fcb694f050c8f5d62947f21ce406ec3
Maeesha <maeesha.rahman@gmail.com>
2019-03-01 18:14:13 -0500

    delete from list button works

--- src/components/SavedMovies.js
+++ src/components/SavedMovies.js
@@ -9,47 +9,53 @@ class SavedMovies extends Component {
         }
     }
 
-    onMovieDelete = () => {
+    onMovieDelete = (key) => {
         console.log('deleted');
         const movie = this.props.movie
-
+       
         // firebase.database().ref(`movies`).push(movie)
-        const dbRef = firebase.database().ref(`movies`);
+        const dbRef = firebase.database().ref(`movies/${key}`);
         // push whatever the user typed in the input to firebase using the firebase push method
         dbRef.remove(movie);
     }
 
     componentDidMount() {
-        const itemsRef = firebase.database().ref();
+        const itemsRef = firebase.database().ref('movies');
         itemsRef.on('value', (saved) => {
-            let movies = saved.val();
-            // console.log(items);
-            movies = movies.movies
+
             let newState = [];
-            for (let item in movies) {
-                newState.push(movies[item]);
-                console.log(movies[item]);
-            }
-            console.log(newState);
-            this.setState({
-                saved: newState
+            
+            saved.forEach(movie => {
+                newState.push({
+                    ...movie.val(),
+                    firebaseId : movie.key
+                })
             });
-        });
-    }
+
+            console.log(newState)
+
+            this.setState({
+                saved : newState
+            })
+            console.log(this.state.saved);
+            })
+
+        };
+    
     // this.state.saved map through array and put it on page 
     render() {
         
         return ( 
             <div>
                 <h2>Your Watchlist</h2>
-            {
+            {   
                 this.state.saved.map((movie) => {
                     return (
                         <div key = {movie.id} className="watchListComponent">
                         <h3>{movie.title}</h3>
                         <p>{movie.overview}</p>
                         <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`Poster for ${movie.title}.`} />
-                        <button onClick={this.onMovieDelete}>Delete from list</button>
+                        <button onClick={()=>this.onMovieDelete(movie.firebaseId)}>Delete from list</button>
                         </div>
                     )
                 })  
